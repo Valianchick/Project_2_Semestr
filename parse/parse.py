9b4a47e1a551fa6dda3c07a80f8bd0e9b11f9782
Valianchick <99838947+Valianchick@users.noreply.github.com>
2022-05-11 15:47:09 +0300

    Create parse.py

---
+++ parse/parse.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://ru.tradingview.com/markets/stocks-russia/market-movers-large-cap/'
+#HEADERS = {}
+
+def get_html(url, params=None):
+    r = requests.get(url, params=params)
+    return r
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('tr', class_='row-arjDAkRm listRow')
+    cars = []
+    for item in items:
+        cars.append({
+            'Название акции':item.find('sup', class_='apply-common-tooltip').get_text(),
+            'Тикер':item.find('a', class_='apply-common-tooltip').get_text()
+        })
+    print(cars)
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print('Ошибка')
+
+
+
+parse()
